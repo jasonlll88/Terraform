ae41593f2469cade3a76ac32f3b9ef51525c0a85
Jheison Rodriguez <jasonlll88@hotmail.com>
2019-05-21 19:57:27 -0500

    fix folders

--- 01-BuildingInfraestructure/README.md
+++
@@ -1,7 +0,0 @@
-For this example we're going to use AWS as a cloud provider, for the purpose of this you should create an account in AWS if you don't have one. 
-
-The files for terraform have the extension *.tf
-
-The example to launch an ec2 instance in AWS is
-
-*launch-EC2-instance.tf*
--- 01-BuildingInfrasEC2/launch-EC2-instance.tf
+++ 01-BuildingInfrasEC2/launch-EC2-instance.tf
@@ -1,15 +1,26 @@
 /* This is the cloud provider
 	These three parameters should be set up with yours 		
-*/
+
 provider "aws" {
   access_key = "ACCESS_KEY_HERE"
   secret_key = "SECRET_KEY_HERE"
   region     = "us-east-2"
 }
+*/
+
+/* This is the cloud provider
+        If don't use parameters of access_key and secret_key it will find for the credentials of AWS in the file ~/.aws/credentials
+*/
+provider "aws" {
+  region     = "us-east-2"
+}
+
+
+
 
 /* This define an AWS ec2 instance to be created	
 */
 resource "aws_instance" "my-first-ec2-instance" {
-  ami           = "ami-2757f631"
+  ami           = "ami-036a034cc552b82e8"
   instance_type = "t2.micro"
 }
